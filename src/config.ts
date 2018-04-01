10f601750214cfc4d8f9225c7a1a83ffb33c2219
Simon Alling <alling.simon@gmail.com>
2018-04-01 20:00:55 +0200

    Refuse to show source of 'View Source' page
    The size of the URI to open becomes very large even at just one level of
    source viewing recursion. Having a lot of browsing history is a serious
    performance issue in iOS, to an extent that we may even have to abandon
    the current URI implementation for this extension to be practically
    usable. This is a simple quick fix which is better than nothing.

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
+const ID_TOKEN = "view-source-for-ios-by-alling"; // inserted into the generated page so it can be identified
+const SELECTOR_DETECT_PLUGIN = "html#" + ID_TOKEN;
 const HLJS_VERSION = "9.12.0";
 const THEME = "hybrid";
 const SPECIAL_PAGES: RegExp[] = [
@@ -9,4 +11,6 @@ export default {
     URL_HLJS_STYLESHEET: `https://cdnjs.cloudflare.com/ajax/libs/highlight.js/${HLJS_VERSION}/styles/${THEME}.min.css`,
     URL_HLJS_SCRIPT: `https://cdnjs.cloudflare.com/ajax/libs/highlight.js/${HLJS_VERSION}/highlight.min.js`,
     SPECIAL_PAGES,
+    ID_TOKEN,
+    SELECTOR_DETECT_PLUGIN,
 };
